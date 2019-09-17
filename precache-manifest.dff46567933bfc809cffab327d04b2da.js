455080538e45192046a64e553bb79fe4e70da0c6
tahaamaidi92 <benhama.taha.33@gmail.com>
2019-09-18 00:17:01 +0100

    Updates

---
+++ precache-manifest.dff46567933bfc809cffab327d04b2da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6451d8817648bf65be03b6af403638c4",
+    "url": "/ToDoListApp/index.html"
+  },
+  {
+    "revision": "5aa8da19d45cc5e07b6c",
+    "url": "/ToDoListApp/static/css/main.55cefbd1.chunk.css"
+  },
+  {
+    "revision": "1dfc76a9fb77eaf3ba9c",
+    "url": "/ToDoListApp/static/js/2.9aedd0a1.chunk.js"
+  },
+  {
+    "revision": "5aa8da19d45cc5e07b6c",
+    "url": "/ToDoListApp/static/js/main.934b3cfa.chunk.js"
+  },
+  {
+    "revision": "82b4387e9f0474699d12",
+    "url": "/ToDoListApp/static/js/runtime~main.ee6279e7.js"
+  }
+]);
