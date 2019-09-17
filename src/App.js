f07d6ceab20d17ccd508fb5534ebc5a210f143bd
tahaamaidi92 <benhama.taha.33@gmail.com>
2019-09-18 00:17:55 +0100

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Todoitems from './components/TodoItems/todoitem.js';
+import Additem from './components/AddItem/additem.js';
+import './App.css'
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      items : [
+        {id:1,name:'Taha',age:27},
+        {id:2,name:'Chokri',age:26},
+        {id:3,name:'Hamdi',age:34}
+      ]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    }
+  }
+  deleteItem=(id)=>{
+    let items=this.state.items.filter(item => {
+      return item.id !== id
+    })
+     this.setState({items})
+  }
+  addItem=(item)=>{
+  item.id=Math.random();
+   let items=this.state.items;
+   items.push(item);
+   this.setState({items:items})
+  }
+  render() { 
+    return ( 
+    <div className="App container">
+      <h1 className="text-center">Todo List</h1>
+      <Todoitems items={this.state.items} deleteItem={this.deleteItem}/>
+      <Additem  addItem={this.addItem}/>
     </div>
-  );
+    
+     );
+  }
 }
-
+ 
 export default App;
---
+++ src/components/TodoItems/todoitem.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './todoitems.css'
+const Todoitems = (props) => {
+
+   const {items , deleteItem}=props;
+   let length = items.length;
+   const ListItems= length ? (
+    items.map(item => {
+        return(
+            <div key={item.id}>
+                <span className="name">{item.name}</span>
+                <span className="age">{item.age}</span>
+                <span className="action icon" onClick={() => deleteItem(item.id)}>&times;</span>
+            </div>
+        )
+    })
+   ): (
+       <p>There is no item to show</p>
+   )
+    
+        return ( 
+            <div className="Listitems">
+            <div>
+               <span className="name title">Name</span> 
+               <span className="age title">Age</span>
+               <span className="action title">Action</span>
+            </div>
+            {ListItems}
+            </div>
+         );
+
+}
+ 
+export default Todoitems;
